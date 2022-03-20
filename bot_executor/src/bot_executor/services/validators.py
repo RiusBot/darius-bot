ecb9122219869b55eed470e5ac7d75462775b239
Tsung <59220877+air160625@users.noreply.github.com>
2022-03-20 13:51:12 +0800

    Add files via upload
    add config options " use_all_collateral " and " only_usd "

--- bot_executor/src/bot_executor/services/validators.py
+++ bot_executor/src/bot_executor/services/validators.py
@@ -77,15 +77,17 @@ def main_validator(f):
             "duplicate",
             "api_key",
             "api_secret",
+            "use_all_collateral",
+            "only_usd"
             # "scalp_entry",
             # "scalp_stop_loss",
             # "scalp_take_profit",
             # "price",
         ]
-        string_fields = ["exchange", "symbol", "action", "order_type", "stop_loss_type", "tale_profit_type", "api_key", "api_secret", "target"]
+        string_fields = ["exchange", "symbol", "action", "order_type", "stop_loss_type", "take_profit_type", "api_key", "api_secret", "target"]
         numeric_fields = ["quantity", "leverage"]
         dict_fields = []
-        bool_fields = ["test", "duplicate"]
+        bool_fields = ["test", "duplicate", "use_all_collateral", "only_usd"]
         enums = {
             "target": {"SPOT", "MARGIN", "FUTURE"},
             "order_type": {"LIMIT", "MARKET"},
@@ -120,12 +122,14 @@ def clean_validator(f):
             "duplicate",
             "api_key",
             "api_secret",
-            "type"
+            "type",
+            "use_all_collateral",
+            "only_usd"
         ]
         string_fields = ["exchange", "type", "order_type", "stop_loss_type", "tale_profit_type", "api_key", "api_secret", "target"]
         numeric_fields = ["quantity", "leverage"]
         dict_fields = []
-        bool_fields = ["test", "duplicate"]
+        bool_fields = ["test", "duplicate", "use_all_collateral", "only_usd"]
         enums = {
             "target": {"SPOT", "MARGIN", "FUTURE"},
             "order_type": {"LIMIT", "MARKET"},
